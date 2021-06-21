ed240b6bb8e1946afaf1fbb3978f58aed21c1f2a
febby <febbynurfitriyani1208@gmail.com>
2021-06-21 13:49:33 +0700

    map

---
+++ map.dart
@@ -0,0 +1,13 @@
+void main() {
+  //map and key values
+  //literals
+
+  var ibuKota = {
+    'Indonesia' : 'Jakarta',
+    'England' : 'London',
+    'China' : 'Beijing',
+    'Germany' : 'Berlin',
+  };
+
+  print(ibuKota);
+}
